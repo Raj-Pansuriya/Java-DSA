b098b38455f4749eabdab426d2a4d7f9b685a3bb
Raj <rajpansuriya40@gmail.com>
2021-11-14 01:07:42 +0530

    Add simple program about inputs and scanner class

---
+++ lectures/4_first_java_program/src/Input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("please provide some input");
+
+        System.out.println(input.next());
+    }
+}
