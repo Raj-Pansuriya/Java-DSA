29914802378044b51ee3eb11b34e4fb7ee49bafc
Raj <rajpansuriya40@gmail.com>
2021-11-15 20:48:08 +0530

    Add input program

--- lectures/4_first_java_program/src/Input.java
+++ lectures/4_first_java_program/src/Input.java
@@ -4,8 +4,20 @@ public class Input {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("please provide some input");
+        System.out.print("Enter your full name: ");
+        String full_name = input.nextLine();
+        System.out.print("Enter your first name: ");
+        String name = input.next();
+        System.out.print("Enter your division: ");
+        String division = input.next();
+        System.out.print("Enter your roll no: ");
+        int rollno = input.nextInt();
+        System.out.print("Enter your maths marks: ");
+        float marks = input.nextFloat();
 
-        System.out.println(input.next());
+        System.out.println("Name : "+full_name);
+        System.out.println("Division : "+division);
+        System.out.println("Roll no. : "+rollno);
+        System.out.print("Marks : "+marks);
     }
 }
