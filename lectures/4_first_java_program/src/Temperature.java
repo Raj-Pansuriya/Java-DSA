57f4fcd473b04e987df75ec119555674ab9e502b
Raj <rajpansuriya40@gmail.com>
2021-11-15 21:52:19 +0530

    resolved an error

--- lectures/4_first_java_program/src/Temperature.java
+++ lectures/4_first_java_program/src/Temperature.java
@@ -4,7 +4,7 @@ public class Temperature {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Please enter temp in C: ");
-        float tempC = in.nextFloat();
+        float tempC = input.nextFloat();
         float tempF = (tempC * 9/5) + 32;
         System.out.println(tempF);
     }
