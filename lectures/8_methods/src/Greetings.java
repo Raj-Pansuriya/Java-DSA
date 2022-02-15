bf0669423ac33e759a1f4375b3b63c3935b2ca04
Raj <rajpansuriya40@gmail.com>
2022-02-15 13:18:34 +0530

    chore: add greet function

---
+++ lectures/8_methods/src/Greetings.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Greetings {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println(greet());
+    }
+
+    static String greet() {
+        String message = "How are You?";
+
+        return message;
+    }
+}
