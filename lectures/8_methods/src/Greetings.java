c990859451e73329674f5391993e61434a323e44
Raj <rajpansuriya40@gmail.com>
2022-02-15 13:22:31 +0530

    feat:add String type parameters to the code

--- lectures/8_methods/src/Greetings.java
+++ lectures/8_methods/src/Greetings.java
@@ -4,7 +4,18 @@ public class Greetings {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        String name;
+        System.out.print("Please enter your name: ");
+        name = input.next();
+
+        String personalized = myGreet(name);
+        System.out.println(personalized);
         System.out.println(greet());
+    }
+
+    static String myGreet(String name) {
+        String message = "Hello " + name;
+        return message;
     }
 
     static String greet() {
