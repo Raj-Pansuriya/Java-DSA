efe2cb5c2e117beb97e8cf7973cdee0d214040c0
Raj <rajpansuriya40@gmail.com>
2022-02-15 11:55:47 +0530

    feat:use arguments to pass numbers to the function

--- lectures/8_methods/src/Sum.java
+++ lectures/8_methods/src/Sum.java
@@ -2,32 +2,33 @@ import java.util.Scanner;
 
 public class Sum {
     public static void main(String[] args) {
-        int ans = sum();
-        System.out.println("Sum of two numbers is: "+ans);
-    }
-
-
-/*
-Syntax of a function
-
-access_modifier return_type name() {
-
-    body
-
-}
-*/
-
-
-    static int sum() {
-        int num1,num2,sum;
         Scanner input = new Scanner(System.in);
 
+        int num1, num2;
         System.out.println("Enter number 1: ");
-        num1=input.nextInt();
+        num1 = input.nextInt();
         System.out.println("Enter number 2: ");
-        num2= input.nextInt();
+        num2 = input.nextInt();
+        int ans = sum(num1, num2);
 
-        sum=num1+num2;
+        System.out.println("Sum of two numbers is: " + ans);
+    }
+
+    /*
+     * Syntax of a function
+     *
+     * access_modifier return_type name(optional arguments) {
+     *
+     * body
+     *
+     * }
+     */
+
+    static int sum(int num1, int num2) {
+        int sum;
+
+        sum = num1 + num2;
+
         return sum;
     }
 }
