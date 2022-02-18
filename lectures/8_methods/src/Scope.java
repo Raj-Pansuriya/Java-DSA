aacda533119f400155ca1ad7c7560bf9a32a7ece
Raj <rajpansuriya40@gmail.com>
2022-02-18 12:56:42 +0530

    chore: add code to explain loop scope

--- lectures/8_methods/src/Scope.java
+++ lectures/8_methods/src/Scope.java
@@ -6,6 +6,8 @@ public class Scope {
             // int a = 30; will give error. You can't initialixe the same var inside block
             // again
             int c = 30;
+            // below line will update the original var. `a` as its using the same reference variable.
+            // We haven't initialized any other `a` hence the block is bound to use the same reference variable.
             a = 5;
             System.out.println("Inside the block, a:" + a);
         }
@@ -13,6 +15,21 @@ public class Scope {
 
         // System.out.println("Outside the block, c:" + c);
         // Above line gives error, c can't be accessed outside of the block.
+
+        int c=100;
+        // As pur code do not know any `c` outside the abouve block, hence we can initialize it again.
+
+
+
+        // loop Scope
+        // loop is just a block along with the looping condition
+        // So all the bounds of a block scope apply to it
+        // additionally if you initialize an the iterating variable `i` in the loop,
+        // you can't use it outside the looping
+        for(int i=0;i<5;i++){
+            System.out.println(i);
+        }
+        // System.out.println(i); Gives an error
     }
 
 }
