21d9cd72c1c5f929d95a272779265b225140cb7c
Raj <rajpansuriya40@gmail.com>
2022-02-18 14:14:13 +0530

    feat:add code to understand carargs with overloading

--- lectures/8_methods/src/Overloading.java
+++ lectures/8_methods/src/Overloading.java
@@ -1,8 +1,33 @@
+import java.util.Arrays;
+
 public class Overloading {
     public static void main(String[] args) {
         function(7);
         System.out.println();
         function("Raj Pansuriya");
+
+        // Sum od integers
+        int ans = 0;
+        ans = sum(1, 2);
+        System.out.println(ans);
+    }
+
+    // sum of 2 integers
+    static int sum(int a, int b) {
+        System.out.println("1st");
+        return a + b;
+    }
+
+    // sum of 3 integers
+    static int sum(int a, int b, int c) {
+        System.out.println("2nd");
+        return a + b + c;
+    }
+
+    // sum of unknown number of integers
+    static int sum(int... i) {
+        System.out.println("3rd");
+        return Arrays.stream(i).sum();
     }
 
     static void function(int a) {
