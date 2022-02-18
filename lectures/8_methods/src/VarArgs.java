56f3cbb4e3810b67162005013b89f29f6f1cae44
Raj <rajpansuriya40@gmail.com>
2022-02-18 13:40:40 +0530

    chore: add code to understand pasing varArgs of different datatypes

--- lectures/8_methods/src/VarArgs.java
+++ lectures/8_methods/src/VarArgs.java
@@ -6,6 +6,16 @@ public class VarArgs {
         // but you do not know the exact number of integrers a user will input.
         // in cases like this you can use `Variable Arguments`
         function(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        // Now let us say we want to pass different types of arguments. We can do that
+        // as follows
+        varTypes(1, 2, "Raj", "Pasnuriya");
+    }
+
+    static void varTypes(int a, int b, String... s) {
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(Arrays.toString(s));
     }
 
     static void function(int... v) {
