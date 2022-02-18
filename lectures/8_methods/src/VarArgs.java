9cd3bde1a1f937447a65068ebc45f1ce1aef132a
Raj <rajpansuriya40@gmail.com>
2022-02-18 13:37:47 +0530

    fix: update function name

--- lectures/8_methods/src/VarArgs.java
+++ lectures/8_methods/src/VarArgs.java
@@ -5,10 +5,10 @@ public class VarArgs {
         // What if you want to print some integers
         // but you do not know the exact number of integrers a user will input.
         // in cases like this you can use `Variable Arguments`
-        sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        function(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
     }
 
-    static void sum(int ...v) {
+    static void function(int... v) {
         System.out.println(Arrays.toString(v));
     }
 }
